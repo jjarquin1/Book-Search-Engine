310755ec1efd5220ad30d5497c1274b93e458c09
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-08-15 16:30:26 -0700

    added mutation.js file to define mutations being exported

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login ($email: STRING!, $passowrd: String!) {
+    login(email:$email, passowrd:$password){
+        token
+        user {
+            _id
+            username
+            email
+        }
+    }
+}`;
+
+export const ADD_USER =gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email:$email, passoword: $password){
+        token
+        user {
+            _id
+            username
+            email
+        }
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookToSave: BookInput) {
+    saveBook(bookToSave: $bookToSave) {
+        _id
+        username
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`;
+
+export const DELETE_BOOK = gql`
+mutation removeBook($bookId:ID!){
+    removeBook(bookId: $bookId){
+        _id
+        username
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title
+            image
+            link
+    }
+}`
