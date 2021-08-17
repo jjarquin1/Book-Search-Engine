1b1810b2939effe3c5c0c984ae18b872b693cd8c
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-08-16 19:45:07 -0700

    imported dependancies and created apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./schemas');
+const {ApolloServer} = require('apollo-server-express');
 
+//importing from schemas
+const db = require('./config/connection');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth')
+
+//creating Express application 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// creating Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// running the Apollo server
+(async () => {
+  await server.start();
+  server.applyMiddleware({app});
+})()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +32,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server listening on port ${PORT}!`);
+    console.log(`Open Graphql at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
